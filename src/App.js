49203f250ca468aa2f2d14341e19f8c78739f79d
Blessing Orazulume <blessing.orazulume@andela.com>
2017-07-10 19:50:47 +0100

    google URL shortener API gets short url from long url

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    linkText: ''
+  }
+
+  shortenLink = () => {
+    const url = `https://www.googleapis.com/urlshortener/v1/url?key=AIzaSyD_orNx_Zw_3SZ7CsuAJB_t9n4RPVB6d9g`;
+    return fetch(url, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({'longUrl': this.state.linkText})
+    })
+      .then((res) => res.json())
+      .then((res) => console.log(res, "res"))
+      .catch((err) => console.log(err, 'should get an error here'))
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <input
+          type="text"
+          placeholder="link here"
+          value={this.state.linkText}
+          onChange={(event) => this.setState({ linkText: event.target.value })} />
+        <button onClick={this.shortenLink}>shorten</button>
       </div>
     );
   }
