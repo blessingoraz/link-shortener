208e9886c75b0b0b0cc33518670742b8e51b5c2a
Blessing Orazulume <blessing.orazulume@andela.com>
2017-07-11 00:04:43 +0100

    add momentjs to format date

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 import './App.css';
 
 class App extends Component {
   state = {
-    linkText: ''
+    linkText: '',
+    linkUrls: []
+  }
+
+  getLinkAnalytics = (id) => {
+    const url = `https://www.googleapis.com/urlshortener/v1/url?key=${process.env.REACT_APP_URL_KEY}&shortUrl=${id}&projection=FULL`
+    return fetch(url)
+      .then((res) => res.json())
+      .then((res) => {
+        let {analytics,created, id, longUrl } = res;
+        let linkObj = {
+          numberOfClicks: analytics.allTime.shortUrlClicks,
+          created: moment().startOf('day').fromNow(),
+          shortUrl: id,
+          longUrl: longUrl
+        }
+        this.state.linkUrls.push(linkObj);
+      })
+      .catch((err) => console.log(err, 'should get an error here'))
   }
 
   shortenLink = () => {
-    const url = `https://www.googleapis.com/urlshortener/v1/url?key=AIzaSyD_orNx_Zw_3SZ7CsuAJB_t9n4RPVB6d9g`;
+    const { linkText, shortUrls } = this.state;
+    const url = `https://www.googleapis.com/urlshortener/v1/url?key=${process.env.REACT_APP_URL_KEY}`;
     return fetch(url, {
       method: "POST",
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({'longUrl': this.state.linkText})
+      body: JSON.stringify({'longUrl': linkText})
     })
       .then((res) => res.json())
-      .then((res) => console.log(res, "res"))
+      .then((res) => {
+        this.getLinkAnalytics(res.id)
+      })
       .catch((err) => console.log(err, 'should get an error here'))
+  }
+
+  showUrl = () => {
+    if(this.state.linkUrls) {
+      console.log(this.state.linkUrls, "hello")      
+    }
   }
 
   render() {
@@ -29,6 +57,8 @@ class App extends Component {
           value={this.state.linkText}
           onChange={(event) => this.setState({ linkText: event.target.value })} />
         <button onClick={this.shortenLink}>shorten</button>
+
+        {this.showUrl()}
       </div>
     );
   }
