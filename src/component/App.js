70ceecb573b7c73b726d11f92528eca4bea8bf0f
Blessing Orazulume <blessing.orazulume@andela.com>
2017-07-11 01:21:18 +0100

    style header and input container

--- src/component/App.js
+++ src/component/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import moment from 'moment';
-import './App.css';
+import '../styles/App.css';
+import Header from './Header';
 
 class App extends Component {
   state = {
@@ -13,7 +14,7 @@ class App extends Component {
     return fetch(url)
       .then((res) => res.json())
       .then((res) => {
-        let {analytics,created, id, longUrl } = res;
+        let { analytics, id, longUrl } = res;
         let linkObj = {
           numberOfClicks: analytics.allTime.shortUrlClicks,
           created: moment().startOf('day').fromNow(),
@@ -26,37 +27,43 @@ class App extends Component {
   }
 
   shortenLink = () => {
-    const { linkText, shortUrls } = this.state;
+    const { linkText } = this.state;
     const url = `https://www.googleapis.com/urlshortener/v1/url?key=${process.env.REACT_APP_URL_KEY}`;
     return fetch(url, {
       method: "POST",
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({'longUrl': linkText})
+      body: JSON.stringify({ 'longUrl': linkText })
     })
       .then((res) => res.json())
       .then((res) => {
-        this.getLinkAnalytics(res.id)
+        this.getLinkAnalytics(res.id);
+        this.setState({linkText: ''})
       })
       .catch((err) => console.log(err, 'should get an error here'))
   }
 
   showUrl = () => {
-    if(this.state.linkUrls) {
-      console.log(this.state.linkUrls, "hello")      
+    if (this.state.linkUrls) {
+      console.log(this.state.linkUrls, "hello")
     }
   }
 
   render() {
     return (
       <div className="container">
-        <input
-          type="text"
-          placeholder="link here"
-          value={this.state.linkText}
-          onChange={(event) => this.setState({ linkText: event.target.value })} />
-        <button onClick={this.shortenLink}>shorten</button>
+        <Header />
+        <div className="inputContainer">
+          <input
+            type="text"
+            placeholder="link here"
+            value={this.state.linkText}
+            onChange={(event) => this.setState({ linkText: event.target.value })} />
+          <button 
+            onClick={this.shortenLink} 
+            className={(this.state.linkText.length === 0) ? "disable" : "enable"}>Shorten this link</button>
+        </div>
 
         {this.showUrl()}
       </div>
